f1e9d746574c8dab86af235b916d403d382163c0
arashi <1119067989@qq.com>
2023-10-15 22:26:48 +0800

    run

--- django/buysms/run.sh
+++ django/buysms/run.sh
@@ -1,11 +1,22 @@
 #!/bin/bash
 
-python3 manage.py runserver 0.0.0.0:8080
+# python3 manage.py runserver 0.0.0.0:8080
 
-# DIR="$( cd "$( dirname "$0" )" && pwd )"
-# echo $DIR
+# 已经存在进程则kill掉
+PID="$(pgrep -f buysms.wsgi)"
+if [[ -n PID ]]; then
+    kill $PID
+fi
 
-# cd $DIR
+echo "waiting process shutdown ..."
+while [[ -n "$PID" ]]
+do
+    PID="$(pgrep -f ww_spam_svr:ww_spam_svr)"
+done
+echo "all process shotdown, begin to run ..."
 
-# # ulimit -n 50000
-# nohup gunicorn --config=buysms/gunicorn_conf.py buysms.wsgi &> /dev/null &
+# ulimit -n 50000
+# buysms.wsgi为进程名
+nohup gunicorn --config=./gunicorn_conf.py buysms.wsgi &> /dev/null &
+
+echo "running success!"
