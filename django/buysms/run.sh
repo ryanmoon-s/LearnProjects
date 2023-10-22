06e6b8c33a42b173559bf20f61bbba5b06dbd69f
arashi <1119067989@qq.com>
2023-10-22 15:20:52 +0800

    run

--- django/buysms/run.sh
+++ django/buysms/run.sh
@@ -11,12 +11,13 @@ fi
 echo "waiting process shutdown ..."
 while [[ -n "$PID" ]]
 do
-    PID="$(pgrep -f ww_spam_svr:ww_spam_svr)"
+    PID="$(pgrep -f buysms.wsgi)"
 done
 echo "all process shotdown, begin to run ..."
 
 # ulimit -n 50000
-# buysms.wsgi为进程名
+# buysms.wsgi为django应用的路径，gunicorn会创建django应用来处理请求
+# 同时也作为进程名
 nohup gunicorn --config=./gunicorn_conf.py buysms.wsgi &> /dev/null &
 
 echo "running success!"
